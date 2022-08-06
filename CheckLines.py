f2f58aa2cabaf194e7b17d2b5235bca3bdf62579
JokNavi <JokNavi1@gmail.com>
2022-08-06 20:18:44 +0200

    removed un-needed files

--- CheckLines.py
+++ CheckLines.py
@@ -2,23 +2,30 @@ import cv2 as cv
 import numpy as np
 import math
 
-src_img = cv.imread('test.png')
-cv.imshow('Original Image',src_img)
+img = cv.imread('OnlyLines.png')
+gray = cv.cvtColor(img,cv.COLOR_BGR2GRAY)
 
-dst_img = cv.Canny(src_img, 50, 200, None, 3)
+kernel_size = 5
+blur_gray = cv.GaussianBlur(gray,(kernel_size, kernel_size),0)
 
-lines = cv.HoughLines(dst_img, 1, np.pi / 180, 150, None, 0, 0)
+low_threshold = 50
+high_threshold = 150
+edges = cv.Canny(blur_gray, low_threshold, high_threshold)
 
-for i in range(0, len(lines)):
-            rho_l = lines[i][0][0]
-            theta_l = lines[i][0][1]
-            a_l = math.cos(theta_l)
-            b_l = math.sin(theta_l)
-            x0_l = a_l * rho_l
-            y0_l = b_l * rho_l
-            pt1_l = (int(x0_l + 1000*(-b_l)), int(y0_l + 1000*(a_l)))
-            pt2_l = (int(x0_l - 1000*(-b_l)), int(y0_l - 1000*(a_l)))
-            cv.line(src_img, pt1_l, pt2_l, (0,0,255), 3, cv.LINE_AA)
+rho = 1  # distance resolution in pixels of the Hough grid
+theta = np.pi / 180  # angular resolution in radians of the Hough grid
+threshold = 15  # minimum number of votes (intersections in Hough grid cell)
+min_line_length = 50  # minimum number of pixels making up a line
+max_line_gap = 20  # maximum gap in pixels between connectable line segments
+line_image = np.copy(img) * 0  # creating a blank to draw lines on
 
-cv.imshow("Image with lines", src_img)
-cv.waitKey(0)
+# Run Hough on edge detected image
+# Output "lines" is an array containing endpoints of detected line segments
+lines = cv.HoughLinesP(edges, rho, theta, threshold, np.array([]),
+                    min_line_length, max_line_gap)
+
+for line in lines:
+    for x1,y1,x2,y2 in line:
+        cv.line(line_image,(x1,y1),(x2,y2),(1,0,0),5)
+    # Draw the lines on the  image
+lines_edges = cv.addWeighted(img, 0.8, line_image, 1, 0)
