604b1880a824f903b7c651b81bdd15913f87e151
JokNavi <JokNavi1@gmail.com>
2022-08-09 19:06:22 +0200

    Fixed ShortestPath

--- Test/FindColour.py
+++
@@ -1,7 +0,0 @@
-import cv2
-def Colour(Input):
-    image = cv2.imread(Input)
-    for y in range(0, 100, 10):
-        for x in range(0, 100, 10):
-            color = image[x, y]
-    print(color)
