89cd49d3249dc12e2aef22ba4fe188b12d511954
JokNavi <JokNavi1@gmail.com>
2022-08-12 19:44:08 +0200

    Fixing Intersect points

--- MainProject/MyFunctions.py
+++ MainProject/MyFunctions.py
@@ -4,8 +4,13 @@ import cv2
 import math
 import json
 import imutils
-import colorsys
+import numpy as np
 import networkx as nx
+import rhinoscriptsyntax as rs
+import matplotlib.pyplot as plt
+from PIL import Image  
+from skimage.morphology import skeletonize
+
 class Colours():
 
     def HexToRgb(self,HEX):
@@ -15,18 +20,96 @@ class Colours():
         return Output
 
     def RgbToHsv(self,RGB):
-        (r, g, b) = (RGB[0], RGB[1], RGB[2])
-        #normalize
-        (r, g, b) = (r / 255, g / 255, b / 255)
-        #convert to hsv
-        (h, s, v) = colorsys.rgb_to_hsv(r, g, b)
-        #expand HSV range
-        (h, s, v) = (int(h * 179), int(s * 255), int(v * 255))
-        Output = h,s,v
-        print('HSV: ', Output)
+        BGR = np.uint8([[RGB]])
+        Output = cv2.cvtColor(BGR,cv2.COLOR_BGR2HSV)
+        print (Output)
         return Output
 
 class ImageManipulation():
+
+    def MakeIntersect(self):
+        def Binary():
+            #Make image binary
+            img=cv2.imread('MainProject\Out\Lines.jpg') 
+            ImGray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY) 
+            thresh = 5
+            im_bw = cv2.threshold(ImGray, thresh, 255, cv2.THRESH_BINARY)[1]
+            ImBinary = np.array(im_bw, dtype=bool)
+            return ImBinary
+
+        def Skeletonise(ImBinary):
+            # Invert the horse image
+            image = ImBinary
+            # perform skeletonization
+            skeleton = skeletonize(image)
+            # display results
+            fig, axes = plt.subplots(nrows=1, ncols=2, figsize=(8, 4),
+                                    sharex=True, sharey=True)
+            ax = axes.ravel()
+            ax[0].imshow(image, cmap=plt.cm.gray)
+            ax[0].axis('off')
+            ax[0].set_title('original', fontsize=20)
+            ax[1].imshow(skeleton, cmap=plt.cm.gray)
+            ax[1].axis('off')
+            ax[1].set_title('skeleton', fontsize=20)
+            fig.tight_layout()
+            plt.show()
+            return skeleton
+            
+        def HitOrMiss(Skeleton):
+            '''input_image = np.array((
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 255, 255, 255, 0, 0, 0, 255],
+    [0, 255, 255, 255, 0, 0, 0, 0],
+    [0, 255, 255, 255, 0, 255, 0, 0],
+    [0, 0, 255, 0, 0, 0, 0, 0],
+    [0, 0, 255, 0, 0, 255, 255, 0],
+    [0,255, 0, 255, 0, 0, 255, 0],
+    [0, 255, 255, 255, 0, 0, 0, 0]), dtype="uint8")'''
+            
+            Skeleton.astype(int)*255 
+            input_image = np.array((Skeleton), dtype="uint8")
+            kernel = np.array((
+                    [0, 0, 0,],
+                    [0, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 0],
+                    [0, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 0],), dtype="int")
+            output_image = cv2.morphologyEx(input_image, cv2.MORPH_HITMISS, kernel)
+            rate = 50
+            kernel = (kernel + 1) * 127
+            kernel = np.uint8(kernel)
+            kernel = cv2.resize(kernel, None, fx = rate, fy = rate, interpolation = cv2.INTER_NEAREST)
+            cv2.imshow("kernel", kernel)
+            cv2.moveWindow("kernel", 0, 0)
+            input_image = cv2.resize(input_image, None, fx = rate, fy = rate, interpolation = cv2.INTER_NEAREST)
+            cv2.imshow("Original", input_image)
+            cv2.moveWindow("Original", 0, 200)
+            output_image = cv2.resize(output_image, None , fx = rate, fy = rate, interpolation = cv2.INTER_NEAREST)
+            cv2.imshow("Hit or Miss", output_image)
+            cv2.moveWindow("Hit or Miss", 500, 200)
+            cv2.waitKey(0)
+            cv2.destroyAllWindows()
+
+        def GoodTrack(): 
+            img = cv2.imread('MainProject\Out\Lines.jpg')
+            gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+            corners = cv2.goodFeaturesToTrack(gray,90,0.01,10)
+            corners = np.int0(corners)
+            for i in corners:
+                x,y = i.ravel()
+                cv2.circle(img,(x,y),3,255,-1)
+            plt.imshow(img),plt.show()
+        
+        #ImBinary = Binary()
+        #Skeleton = Skeletonise(ImBinary)
+        #HitOrMiss(Skeleton)
+        #GoodTrack()
+        def RhinoSol():
+            obj = rs.GetObject("Select a line")
+            if rs.IsLine(obj):
+                print("The object is a line.")
+            else:
+                print ("The object is not a line.")
+        RhinoSol()
 
     def Center(self, Input):
         # load the image, convert it to grayscale, blur it slightly,
@@ -57,33 +140,21 @@ class ImageManipulation():
             SendToFile[Counter]= (int(cX),int(cY))
             Counter = Counter+1
             print(Counter)
-        with open('MainProject\Out\CoordsGreenDots.txt', 'w') as file:
-            file.write(json.dumps(SendToFile))
-        return SendToFile
-
-    def Mask(self, Input, Colour):
-        img = cv2.imread(Input)
-        ## mask of red color
-        C = Colours()
-        Colour = C.HexToRgb(Colour)
-        Colour = C.RgbToHsv(Colour)
-        mask = cv2.inRange(img, (0,0,200),(Colour))
-        ## mask of blue color
-        res = cv2.bitwise_and(img,img,mask= mask)
-        ## final mask
-        cv2.imwrite(Colour, res)
-
-class Paths():
-
+        cv2.imshow('image',image)
+        cv2.waitKey(0)
+        with open('MainProject\Out\CoordsGreenDots.json', 'w') as File:
+            File.write(json.dumps(SendToFile))
+            File.close()
+            return SendToFile
+class Paths():  
     def Length(self,InputOne,InputTwo):
         #0,1 | x1, y1
         #1,0 | x2, y2
         #dist = math.sqrt((x2 - x1)**2 + (y2 - y1)**2)
         Dist = math.sqrt((int(InputTwo[0]) - int(InputOne[0]))**2 + (int(InputTwo[1]) - int(InputOne[1]))**2) 
         return Dist 
-
-    def ShortestPath(self,Nodes,Start,End):
-        G = nx.Graph(Nodes)
+    def ShortestPath(self,Start,End,Nodes):
+        G = nx.Graph()
         G.add_edge("0", "1", weight=4)
         G.add_edge("1", "3", weight=2)
         G.add_edge("0", "2", weight=3)
@@ -93,15 +164,103 @@ class Paths():
 class CheckPaths():
     def __init__(self,Im):
         self.Im = Im
+
+    def Size(self):
+        return self.Im.size
+
     def FindColour(self, X, Y):
             pix = self.Im.load()
-            print("Image size: ",self.Im.size)  # Get the width and hight of the image for iterating over # Get the RGBA Value of the a pixel of an image
             return pix[X,Y]
 
     def ReplaceColour(self,X,Y):
             pix = self.Im.load()
             pix[X,Y] = 255,255,255
             self.Im.save('MainProject\Out\Modified.png')
+    def FindEdges(self, Dot, Check):
+        with open('MainProject\Out\CoordsGreenDots.json') as File:
+            Coords = json.load(File)
+        File.close()
+        Coord = Coords[f'{Dot}']
+        Minus = lambda a : int(a) - 15
+        Plus = lambda a : int(a) + 15
+        #Output = (0,0,0)
+        if '-X' in Check and CheckPaths.FindColour(self,Minus(Coord[0]),Coord[1])>(20,20,20):
+            return '-X'
 
-if (__name__ == '__main__'):
+        if '+X' in Check and CheckPaths.FindColour(self,Plus(Coord[0]),Coord[1])>(20,20,20):
+                return '+X'
+
+        if '-Y' in Check and CheckPaths.FindColour(self,Coord[0],Minus(Coord[1]))>(20,20,20):
+                return '-Y'
+
+        if '+Y' in Check and CheckPaths.FindColour(self,Coord[0],Plus(Coord[1]))>(20,20,20):
+                return '+Y'
+
+        else: return 'None'
+            
+    def ClosestDot(self, Dot, Facing):
+        with open('MainProject\Out\CoordsGreenDots.json') as File:
+            Coords = json.load(File)
+        File.close()
+        Coord = Coords[f'{Dot}']
+        #print(Coord)
+        FoundGreen = False
+
+        if Facing == '-X':
+            Coord[0] = Coord[0] -8
+            while FoundGreen == False:
+                if  Coord[0] < 0: break
+                Coord[0] = Coord[0] -10
+                Colour = CheckPaths.FindColour(self,Coord[0],Coord[1])
+                if((Colour[1] >240)):
+                    FoundGreen = True
+                #print(str(Coord[0]))
+                CheckPaths.ReplaceColour(self,Coord[0],Coord[1])
+                return Coord[0]
+
+        elif Facing == '+X':
+            Coord[0] = Coord[0] +8
+            while FoundGreen == False:
+                if  Coord[0] < 0: break
+                Coord[0] = Coord[0] +10
+                Colour = CheckPaths.FindColour(self,Coord[0],Coord[1])
+                if((Colour[1] >240)):
+                    FoundGreen = True
+                #print(str(Coord[0]))
+                CheckPaths.ReplaceColour(self,Coord[0],Coord[1])
+                return Coord[0]
+
+        elif Facing == '-Y':
+            Coord[1] = Coord[1] -8
+            while FoundGreen == False:
+                if  Coord[1] < 0: break
+                Coord[1] = Coord[1] -10
+                Colour = CheckPaths.FindColour(self,Coord[0],Coord[1])
+                if((Colour[1] >240)):
+                    FoundGreen = True
+                #print(str(Coord[1]))
+                CheckPaths.ReplaceColour(self,Coord[0],Coord[1])
+                return Coord[1]
+
+        elif Facing == '+Y':
+            Coord[1] = Coord[1] +8
+            while FoundGreen == False:
+                if  Coord[1] < 0: break
+                Coord[1] = Coord[1] +10
+                Colour = CheckPaths.FindColour(self,Coord[0],Coord[1])
+                if((Colour[1] >240)):
+                    FoundGreen = True
+                #print(str(Coord[1]))
+                CheckPaths.ReplaceColour(self,Coord[0],Coord[1])
+                return Coord[1]
+
+    def FindClosest(self,Axis,SpecialCoord):
+        with open('MainProject\Out\CoordsGreenDots.json') as File:
+            Coords = json.load(File)
+        
+        
+        #myArr[myArr < myNumber].max()
+
+ 
+if(__name__ == '__main__'):
     sys.exit()
--- MainProject/RunProgram.py
+++ MainProject/RunProgram.py
@@ -3,22 +3,44 @@ import json
 from PIL import Image
 from MyFunctions import ImageManipulation, Colours, CheckPaths, Paths
 
-#CALCULATE SHORTEST PATH
-with open("MainProject\Out\CoordsGreenDots.txt", 'r', encoding='utf-8') as File:
-    Coords = json.load(File)
-
+#TESTING
 I = ImageManipulation()
-#I.Mask('MainProject\Out\GreenAndRed.png','ff0000')
-#I.Center('MainProject\Out\OnlyGreenPoints.png')
+#I.Center('MainProject\Out\Intersects.jpg')
+I.MakeIntersect()
 
 C = Colours()
-#C.RgbToHsv([255,0,0])
-#C.HexToRgb('#ff0000')
+#C.RgbToHsv([250,0,4])
+#C.HexToRgb('#0000ff')
 
-Im = Image.open('MainProject\Out\LinesOnlyBlack.png')
+Im = Image.open('MainProject\Out\IntersectAndLines.jpg')
 CH = CheckPaths(Im)
-CH.ReplaceColour(0,0)
-CH.FindColour(0,0)
+#print(CH.Size())
+#print(CH.FindColour(0,0))
+#print(CH.ReplaceColour(0,0))
+
+P = Paths()
+##P.ShortestPath(0,3,)
+
+CP = CheckPaths(Im)
+Directions = ['-X','+X','-Y','+Y']
+Dot = 1
+#CP.Size()
+#print(CP.FindColour(877,782))
+#Facing =  CP.FindEdges(Dot,Directions)
+#Axis = CP.ClosestDot(1,Facing)
+#print(CP.FindClosest(Facing,Axis))
+
+#PROGRAM
+'''Dot = 1
+Counter = 0
+Directions = ['-X','+X','-Y','+Y']
+while Counter < 4:
+    Facing =  CP.FindEdges(Dot,Directions)
+    print(f'Facing: {Facing}')
+    if Facing in Directions:
+        print(f'Closest Dot: {CP.ClosestDot(Dot,Facing)}')
+        Directions.remove(Facing)
+    Counter = Counter + 1'''
 
 if (__name__ == '__main__'):
     sys.exit()
